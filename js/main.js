1990fa0a745f6aa06ca68459388c2b3dd77ac2d2
Ria Virk <riausman11@gmail.com>
2024-05-02 00:49:34 +0200

    11th Commit

--- js/main.js
+++ js/main.js
@@ -12,4 +12,42 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
     slidesPerView: 2,
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        960: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+    }
 })
+// const swiper = new Swiper(".swiper", {
+//     effect: "cards",
+//     grabCursor: true,
+//   });
+
+//   const swiper = new Swiper(".swiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//       rotate: 50,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1,
+//       slideShadows: true,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//   });
+
+// const swiper = new Swiper(".swiper", {
+//     watchSlidesProgress: true,
+//     slidesPerView: 3,
+//   });
